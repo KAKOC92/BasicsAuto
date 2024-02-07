8a2e71103e8e9f7b76baa759e8fa337be92bd57c
Бабкин Алексей <babkin.92@mail.ru>
2024-02-07 19:28:57 +0300

    Second test added

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -16,4 +16,15 @@ public class CashbackHackServiceTest {
 
        assertEquals(actual, expected);
     }
+
+    @Test
+    public void shouldAdvice0IfBuy1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
 }
